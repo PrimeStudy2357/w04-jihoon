ece52b0a9b64e98b0500b3e02a0afdf6a03dc993
Ji-hoon Lee <anteater1056@gmail.com>
2024-02-12 18:11:54 +0900

    Axios 실습

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const { default: axios } = require("axios");
+
+axios.get("http://youtube.com/").then((res) => {
+  console.log(res.data);
+  console.log(res.status);
+});
